7fef4d4569a55515c5d372b3c3e40cb406af8243
aueskinj <71489113+aueskinj@users.noreply.github.com>
2022-09-19 21:19:06 +0300

    reset_to_98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,8 @@
 /**
  * 0-reset_to_98() -  takes a pointer to an int as parameter and 
  * updates the value it points to to 98
- *  returns void
+ * 
+ * returns void
  */
 
  void reset_to_98(int *n)
