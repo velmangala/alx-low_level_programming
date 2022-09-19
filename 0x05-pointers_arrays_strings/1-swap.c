58c885b32e9a593015d2db3e859f0b56625c40ca
aueskinj <71489113+aueskinj@users.noreply.github.com>
2022-09-19 21:49:36 +0300

    quiz 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ *  
+ */
+void swap_int(int *a, int *b)
+{
+    *a=&b;
+    *b=&a;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,2 +1,3 @@
 int main(void);
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
